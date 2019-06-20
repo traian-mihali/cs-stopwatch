70350605fc113bb0db233e1f8ab03f908b9a6499
Traian <traianmihali696@gmail.com>
2019-06-20 12:21:21 +0300

    stopwatch implementation in cs

---
+++ MyStopWatch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class MyStopWatch
+    {
+        private bool _isRunning;
+        private DateTime _start;
+        private DateTime _stop;
+
+        public void Start()
+        {
+            if (_isRunning)
+                throw new InvalidOperationException("[StopWatch] StopWatch is already running.");
+
+            _isRunning = true;
+            _start = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            if (!_isRunning)
+                throw new InvalidOperationException("[StopWatch] StopWatch is not running.");
+
+            _isRunning = false;
+            _stop = DateTime.Now;
+            Duration += _stop - _start;
+        }
+
+        public TimeSpan Duration { get; private set; }
+
+        public void Reset()
+        {
+            Duration = TimeSpan.Zero;
+            _isRunning = false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new MyStopWatch();
+            Run(stopWatch);
+        }
+
+        public static void Run(MyStopWatch stopWatch)
+        {
+            Console.WriteLine("***StopWatch App***");
+
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("StopWatch Commands: ");
+            Console.WriteLine("<START> - starts the SW");
+            Console.WriteLine("<STOP> - stops the SW");
+            Console.WriteLine("<RESET> - resets the SW");
+            Console.WriteLine("<EXIT> - exits the SW");
+            Console.WriteLine("-----------------------");
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("[StopWatch]: ");
+                    var input = Console.ReadLine();
+                    if (input.Trim().ToLower() == "start")
+                    {
+                        stopWatch.Start();
+                        Console.WriteLine("[StopWatch] <STARTED>");
+                    }
+                    else if (input.Trim().ToLower() == "stop")
+                    {
+                        stopWatch.Stop();
+                        Console.WriteLine("[StopWatch] <STOPPED>");
+                        Console.WriteLine("[StopWatch] Duration: " + stopWatch.Duration + " seconds");
+                    }
+                    else if (input.Trim().ToLower() == "reset")
+                    {
+                        stopWatch.Reset();
+                        Console.WriteLine("[StopWatch] <RESET>");
+                        Console.WriteLine("[StopWatch] Duration: " + stopWatch.Duration + " seconds");
+                    }
+                    else if (input.Trim().ToLower() == "exit")
+                    {
+                        Console.WriteLine("[StopWatch] <SHUTTING DOWN>");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("[StopWatch] <INVALID COMMAND>");
+                    }
+                }
+                catch (Exception Error)
+                {
+                    Console.WriteLine(Error.Message);
+                }
+            }
+        }
+    }
+}
